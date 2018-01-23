042acf69e66191c4cb0c40cdf4d977586dc5cf03
Deepen <deepen@lintelindia.com>
2018-01-23 11:31:34 +0530

    Add restaurant list view, model and html file.

--- restaurants/models.py
+++ restaurants/models.py
@@ -6,3 +6,8 @@ from django.db import models
 class RestaurantLocation(models.Model):
     name = models.CharField(max_length=120)
     location = models.CharField(max_length=120, null=True, blank=True)
+    category = models.CharField(max_length=120, null=True, blank=True)
+    timestamp = models.DateTimeField(auto_now=False)
+
+    def __str__(self):
+        return self.name
--- restaurants/views.py
+++ restaurants/views.py
@@ -3,69 +3,80 @@ from django.http import HttpResponse
 import random
 from django.views import View
 from django.views.generic import TemplateView
-# Create your views here.
-
+# # Create your views here.
 #
-# def home(request):
-#     # html_var = 'f string'
-#     # html_ = """<!DOCTYPE html>
-#     # <html lang=en>
-#     # <head> </head>
-#     # <body>
-#     # <h1> Hello World!</h1>
-#     # <p> This is {html_var} coming through</p>
-#     # </body>
-#     # </html>
-#     # """
-#     num = random.randint(0, 1000000)
-#     some_list = [num, random.randint(0, 1000000), random.randint(0, 1000000), random.randint(0, 1000000)]
-#     context = {
-#         'bool_item': False,
-#         'num': num,
-#         'some_list': some_list
-#     }
-#     return render(request, 'home.html', context)
-#     # return HttpResponse(html_)
+# #
+# # def home(request):
+# #     # html_var = 'f string'
+# #     # html_ = """<!DOCTYPE html>
+# #     # <html lang=en>
+# #     # <head> </head>
+# #     # <body>
+# #     # <h1> Hello World!</h1>
+# #     # <p> This is {html_var} coming through</p>
+# #     # </body>
+# #     # </html>
+# #     # """
+# #     num = random.randint(0, 1000000)
+# #     some_list = [num, random.randint(0, 1000000), random.randint(0, 1000000), random.randint(0, 1000000)]
+# #     context = {
+# #         'bool_item': False,
+# #         'num': num,
+# #         'some_list': some_list
+# #     }
+# #     return render(request, 'home.html', context)
+# #     # return HttpResponse(html_)
+# #
+# #
+# # def about(request):
+# #     context = {}
+# #     return render(request, 'about.html', context)
+# #
+# #
+# # def contact(request):
+# #     context = {}
+# #     return render(request, 'contact.html', context)
+# #
+# #
+# # class ContactView(View):
+# #
+# #     def get(self, request, *args, **kwargs):
+# #         context = {}
+# #         return render(request, 'contact.html', context)
 #
 #
-# def about(request):
-#     context = {}
-#     return render(request, 'about.html', context)
+# class HomeView(TemplateView):
+#
+#     template_name = 'home.html'
+#
+#     def get_context_data(self, *args, **kwargs):
+#         context = super(HomeView, self).get_context_data(*args, **kwargs)
+#         print(context)
+#         num = random.randint(0, 1000000)
+#         some_list = [num, random.randint(0, 1000000), random.randint(0, 1000000), random.randint(0, 1000000)]
+#         context = {
+#             'bool_item': False,
+#             'num': num,
+#             'some_list': some_list
+#         }
+#         return context
 #
 #
-# def contact(request):
-#     context = {}
-#     return render(request, 'contact.html', context)
+# class AboutView(TemplateView):
+#     template_name = 'about.html'
 #
 #
-# class ContactView(View):
+# class ContactView(TemplateView):
 #
-#     def get(self, request, *args, **kwargs):
-#         context = {}
-#         return render(request, 'contact.html', context)
+#     template_name = 'contact.html'
+
+from .models import RestaurantLocation
 
 
-class HomeView(TemplateView):
-
-    template_name = 'home.html'
-
-    def get_context_data(self, *args, **kwargs):
-        context = super(HomeView, self).get_context_data(*args, **kwargs)
-        print(context)
-        num = random.randint(0, 1000000)
-        some_list = [num, random.randint(0, 1000000), random.randint(0, 1000000), random.randint(0, 1000000)]
-        context = {
-            'bool_item': False,
-            'num': num,
-            'some_list': some_list
-        }
-        return context
-
-
-class AboutView(TemplateView):
-    template_name = 'about.html'
-
-
-class ContactView(TemplateView):
-
-    template_name = 'contact.html'
+def restaurants_list_view(request):
+    template_name = 'restaurants/restaurants_list.html'
+    query_set = RestaurantLocation.objects.all()
+    context = {
+        'object_list': query_set
+    }
+    return render(request, template_name, context)
